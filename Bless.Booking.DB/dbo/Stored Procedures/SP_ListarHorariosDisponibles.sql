2369c08ed402c306773094acbe8b6255e14d6cb7
douglas-i <hdouglasu@gmail.com>
2025-05-11 12:31:41 -0600

    Add stored procedures

---
+++ Bless.Booking.DB/dbo/Stored Procedures/SP_ListarHorariosDisponibles.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[sp_ListarHorariosDisponibles]
+	@BarberoId INT,
+    @Fecha DATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT HorarioId, Hora
+    FROM HorariosDisponibles
+    WHERE BarberoId = @BarberoId
+      AND Fecha = @Fecha
+      AND Disponible = 1
+    ORDER BY Hora;
+END;
---
+++ Bless.Booking.DB/dbo/Stored Procedures/sp_Reserva_Guardar.sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE [dbo].[sp_Reserva_Guardar]
+	@ClienteId INT,
+    @BarberoId INT,
+    @ServicioId INT,
+    @Fecha DATE,
+    @Hora TIME
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        -- Insertar reserva
+        INSERT INTO Reservas (ClienteId, BarberoId, ServicioId, Fecha, Hora)
+        VALUES (@ClienteId, @BarberoId, @ServicioId, @Fecha, @Hora);
+
+        -- Marcar horario como no disponible
+        UPDATE HorariosDisponibles
+        SET Disponible = 0
+        WHERE BarberoId = @BarberoId AND Fecha = @Fecha AND Hora = @Hora;
+
+        COMMIT;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK;
+        THROW;
+    END CATCH
+END;
