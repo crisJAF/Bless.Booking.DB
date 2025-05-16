f7397bdd5cd2873fce1fda1272611f289fbee483
douglas-i <hdouglasu@gmail.com>
2025-05-15 18:31:40 -0600

    Feat: add version 3 for reservations and available schedules

---
+++ Bless.Booking.DB/dbo/Stored Procedures/V3/fn_GenerarCupos.sql
@@ -0,0 +1,24 @@
+﻿CREATE FUNCTION dbo.fn_GenerarCupos
+(
+    @HoraInicio TIME,
+    @HoraFin TIME,
+    @Duracion INT
+)
+RETURNS @Cupos TABLE (HoraDesde TIME, HoraHasta TIME)
+AS
+BEGIN
+    DECLARE @Actual TIME = @HoraInicio;
+
+    WHILE DATEADD(MINUTE, @Duracion, @Actual) <= @HoraFin
+    BEGIN
+        INSERT INTO @Cupos(HoraDesde, HoraHasta)
+        VALUES (
+            @Actual,
+            DATEADD(MINUTE, @Duracion, @Actual)
+        );
+
+        SET @Actual = DATEADD(MINUTE, @Duracion, @Actual);
+    END
+
+    RETURN;
+END;
---
+++ Bless.Booking.DB/dbo/Table/V3/BarberoHorarioV3.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[BarberoHorarioV3]
+(
+	BarberoHorarioID INT PRIMARY KEY IDENTITY,
+    BarberoID INT NOT NULL,
+    Dia INT NOT NULL, -- 1 = domingo, 2 = lunes, etc.
+    HoraInicio TIME NOT NULL,
+    HoraFin TIME NOT NULL,
+    DuracionCupoMinutos INT NOT NULL DEFAULT 30,
+    Estado BIT NOT NULL DEFAULT 1,
+    FOREIGN KEY (BarberoID) REFERENCES Barbero(BarberoID)
+)
