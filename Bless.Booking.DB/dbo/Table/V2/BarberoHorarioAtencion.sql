cc1c1161ce4e20950500a731f8c4a9452c5773de
douglas-i <hdouglasu@gmail.com>
2025-05-17 18:10:21 -0600

    Refactor: remove version 2 of reservations and available schedules

--- Bless.Booking.DB/dbo/Table/V2/BarberoHorarioAtencion.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[BarberoHorarioAtencion]
-(
-	[BarberoHorarioAtencionID] INT NOT NULL PRIMARY KEY,
-	[BarberoID] INT NOT NULL,
-	[Dia] INT NOT NULL,
-	[HoraDesde] TIME(0) NOT NULL,
-	[HoraHasta] TIME(0) NOT NULL,
-	[Estado] BIT NOT NULL,
-)
